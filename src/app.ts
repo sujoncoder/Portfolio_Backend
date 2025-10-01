5de9df026c7d380de0d912a57e247ac1fb22eee8
sujoncoder <sujonsheikh.in@gmail.com>
2025-10-01 14:30:36 +0600

    database and server setup done

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import express, { type Request, type Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { HTTP_STATUS } from "./constants/httpStatus";
+import notFound from "./middlewar/notFound";
+
+
+const app = express();
+
+
+// APPLICATION LEVEL MIDDLEWARE
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+
+// APPLICATION ROUTE MIDDLEWARE
+// app.use("/api/v1");
+
+
+// APPLICATION ROOT ROUTE
+app.use("/", (req: Request, res: Response) => {
+    res.status(HTTP_STATUS.OK).json({
+        status: "Success",
+        message: "Wellcome to my portfolio server 🚀"
+    })
+});
+
+
+// NOT-FOUND ROUTE
+app.use(notFound);
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import { Server } from "http";
+import app from "./app"
+import { SECRET } from "./config/env";
+import connectDB from "./config/database";
+import gracefulShutdown, { setServer } from "./config/shutdown";
+
+
+let server: Server;
+
+// START SERVER FUNCTION
+const startServer = async () => {
+    await connectDB();
+    server = app.listen(SECRET.PORT, () => {
+        console.log(`➡️  Server is running on http://localhost:${SECRET.PORT}`)
+    });
+
+    setServer(server);
+};
+
+
+// CALL SYNCHRONIZING ===> FIRST START SERVER THEN SEEDING SUPER ADMIN
+(async () => {
+    await startServer();
+    // await seedAdmin();
+})();
+
+
+// UNHANDLE REJECTION ERROR
+process.on("uncaughtException", (error) => gracefulShutdown("Uncaught Exception", error));
+
+// UNCAUGHT REJECTION ERROR
+process.on("unhandledRejection", (error) => gracefulShutdown("Unhandled Rejection", error));
+
+// SIGNAL TERMINATION SIGTERM
+process.on("SIGTERM", () => gracefulShutdown("SIGTERM"));
+process.on("SIGINT", () => gracefulShutdown("SIGINT"));
